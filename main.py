8a3abbbaddafc8068e223388f1d72fd1941091b5
Mary <chudinova-olimp@ya.ru>
2021-11-20 22:19:41 +0500

    work no 1 - espresso

---
+++ main.py
@@ -0,0 +1,45 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("UI1.ui", self)
+        self.con = sqlite3.connect("coffee.db")
+        self.name.setText('coffee.db')
+        self.btn_load.clicked.connect(self.update_result)
+        self.tableWidget.setColumnWidth(0, 20)
+        self.tableWidget.setColumnWidth(1, 100)
+        self.tableWidget.setColumnWidth(2, 115)
+        self.tableWidget.setColumnWidth(3, 115)
+        self.tableWidget.setColumnWidth(4, 115)
+        self.tableWidget.setColumnWidth(5, 75)
+        self.tableWidget.setColumnWidth(6, 100)
+
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+# будет выводить понятно ошибку
+def expert_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    sys.__excepthook__ = expert_hook
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
+
+# pip install -r requirements.txt
